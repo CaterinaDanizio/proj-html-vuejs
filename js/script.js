0510667afbd6430e639d880d6187fa8fcaa2a268
Caterina <danizio@outlook.it>
2020-12-09 16:21:03 +0100

    Inserimento commenti strutturali e materiali progetto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// CONSEGNA
+// Aggiungere Header e Footer con VueJs creando una struttura dati che consenta di inserire i link nelle navbar.
+// Su questo non ci sono API da interrogare, ma create i dati a mano nel vostro js.
+
+
+// Istanza Vue js
+var app = new Vue({
+  el: '#app',
+  data: {
+    
+  }
+})
